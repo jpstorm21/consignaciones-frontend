35c1f974bb5425cd71966b6584aaea411c01bcaf
jpstorm21 <jpstorm21@gmail.com>
2020-09-30 09:48:41 -0300

    version final capstone 2020-1

---
+++ docs/precache-manifest.47d916466420f7e5ba19df533baff28f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446b9a1029d4f64d13e2d1186cb90409",
+    "url": "/index.html"
+  },
+  {
+    "revision": "171b610cb7cd733ae182",
+    "url": "/static/js/2.7b1605cd.chunk.js"
+  },
+  {
+    "revision": "85ad16fb241c18abeaf0c0d1ecb0b3a8",
+    "url": "/static/js/2.7b1605cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4762f2e96654cf0ebe0a",
+    "url": "/static/js/main.e5ae04bc.chunk.js"
+  },
+  {
+    "revision": "f7beae9cea24b9a639b3",
+    "url": "/static/js/runtime-main.afb31aad.js"
+  }
+]);
